37ed5d77c5290cec753fd5d449e68fe37dc0dacd
Jonathan Siao <jonathansiao@gmail.com>
2016-10-22 13:02:06 +0100

    Initial Commit

---
+++ SwiftRingSpinnerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftRingSpinnerView
+//
+//  Created by Jonathan Siao on 22/10/2016.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
